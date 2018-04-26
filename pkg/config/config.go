c9c8b14ee748b224cdb17c191f4d63bcc3a86ea7
cheng.jiannneng@vpclub.cn <cheng.jianneng@vpclub.cn>
2018-04-26 09:58:06 +0800

    Code Format

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,16 +1,16 @@
 package config
 
 type Cluster struct {
-	Cluster string 		`json:"cluster"`
-	Username string 	`json:"username"`
-	Token string    	`json:"token"`
+	Cluster  string `json:"cluster"`
+	Username string `json:"username"`
+	Token    string `json:"token"`
 }
 
 type Hicli struct {
-	Clusters   []Cluster `json:"clusters"`
-	LastIndex  int		 `json:"lastIndex"`
+	Clusters  []Cluster `json:"clusters"`
+	LastIndex int       `json:"lastIndex"`
 }
 
 type Configuration struct {
-	 Hicli Hicli  		 `mapstructure:"hicli"`
+	Hicli Hicli `mapstructure:"hicli"`
 }
